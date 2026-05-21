165698cf084a61970a3657130dc939dc83f409f5
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-21 16:09:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-# ✅ Fix
 ADMINS = list(map(int, os.getenv("ADMINS", "").split()))
 
 # Validate on startup
