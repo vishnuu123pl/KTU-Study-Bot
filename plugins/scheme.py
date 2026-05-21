c2d391fa56c999826d95c8bed9317bbffa66f042
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-21 15:35:08 +0530

    Update scheme.py

--- plugins/scheme.py
+++ plugins/scheme.py
@@ -1,16 +1,44 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# Triggered by any of the 4 main category buttons: cat_materials, cat_pyq, cat_series, cat_model
-@Client.on_callback_query(filters.regex(r"^cat_(\w+)$"))
+
+@Client.on_callback_query(
+    filters.regex(r"^cat_(\w+)$")
+)
 async def scheme(_, query):
-    cat = query.data.split("_", 1)[1]   # materials | pyq | series | model
+
+    cat = query.data.split("_",1)[1]
 
     buttons = InlineKeyboardMarkup([
-        [InlineKeyboardButton("📘 2024 Scheme", callback_data=f"scheme_2024_{cat}")],
-        [InlineKeyboardButton("📗 2019 Scheme", callback_data=f"scheme_2019_{cat}")],
-        [InlineKeyboardButton("⬅ Back",         callback_data="back_start")],
+        [
+            InlineKeyboardButton(
+                "📘 2024 Scheme",
+                callback_data=f"scheme_2024_{cat}"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "📗 2019 Scheme",
+                callback_data=f"scheme_2019_{cat}"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "⬅ Back",
+                callback_data="back_home"
+            )
+        ]
     ])
 
-    await query.message.edit_text("📚 **Select KTU Scheme** 👇", reply_markup=buttons)
+    try:
+
+        await query.message.edit_text(
+            "📚 **Select KTU Scheme** 👇",
+            reply_markup=buttons
+        )
+
+    except:
+
+        pass
+
     await query.answer()
