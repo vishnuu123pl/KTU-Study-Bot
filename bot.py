293fdaf41ece5ceb2f384660467bc4858435da47
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-21 17:24:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN, ADMINS
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Client(
     "KTUStudyBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root=os.path.join(BASE_DIR, "plugins"))
+    plugins=dict(root="plugins")
 )
 
 
