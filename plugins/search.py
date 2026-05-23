673a3e5d3c9c690e6b10188b26ee768302ec545f
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-23 19:17:55 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -30,18 +30,22 @@ async def search(_, message):
                 if query in subject.lower():
 
                     results.append(
-                        f"📚 {subject}\n"
-                        f"🏫 {branch.upper()} | {sem.upper()}"
+                        f"📚 <b>{subject}</b>\n"
+                        f"🏫 {branch.upper()} • "
+                        f"{sem.upper()}"
                     )
 
     if not results:
 
         await message.reply_text(
-            "⚠️ No subject found."
+            "⚠️ No matching subjects found."
         )
 
         return
 
-    await message.reply_text(
+    text = (
+        "🔎 <b>Search Results</b>\n\n" +
         "\n\n".join(results[:15])
     )
+
+    await message.reply_text(text)
