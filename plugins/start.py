dcec798b4150052917fd1357b0d19de904090eaa
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-23 19:35:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,9 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
 from database.models import add_user
 from config import LOG_CHANNEL, ADMINS
 
@@ -26,7 +30,7 @@ START_BUTTONS = InlineKeyboardMarkup([
     [
         InlineKeyboardButton(
             "🔎 Search Subject",
-            switch_inline_query_current_chat=""
+            switch_inline_query_current_chat="/search "
         )
     ],
 
@@ -35,6 +39,7 @@ START_BUTTONS = InlineKeyboardMarkup([
             "ℹ️ About",
             callback_data="about"
         ),
+
         InlineKeyboardButton(
             "📩 Suggest Resource",
             callback_data="request_menu"
@@ -43,30 +48,42 @@ START_BUTTONS = InlineKeyboardMarkup([
 
     [
         InlineKeyboardButton(
-            "📈 Trending Requests",
-            callback_data="top_requests"
+            "💻 Source Code",
+            url="https://github.com/vishnuu123pl/KTU-Study-Bot-V3"
         )
     ]
+
 ])
 
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(
+    filters.command("start")
+)
 async def start(client, message):
 
     user_id = message.from_user.id
 
+    # Save user
     try:
-        await add_user(user_id)
+
+        await add_user(
+            user_id
+        )
 
     except Exception as e:
-        print(f"ADD USER ERROR: {e}")
 
+        print(
+            f"ADD USER ERROR: {e}"
+        )
+
+    # Log new users
     try:
 
         if user_id not in ADMINS:
 
             await client.send_message(
                 LOG_CHANNEL,
+
                 f"#NewUser\n\n"
                 f"ID - {user_id}\n"
                 f"Name - {message.from_user.first_name}"
@@ -74,9 +91,19 @@ async def start(client, message):
 
     except Exception as e:
 
-        print(f"LOG ERROR: {e}")
+        print(
+            f"LOG ERROR: {e}"
+        )
 
-    await message.reply_text(
-        START_TEXT,
+    # Start message with image
+    await message.reply_photo(
+
+        photo=(
+            "https://pic-link-bot.lovable.app/i/"
+            "telegram-1779366829596-64036ff9.jpg"
+        ),
+
+        caption=START_TEXT,
+
         reply_markup=START_BUTTONS
     )
